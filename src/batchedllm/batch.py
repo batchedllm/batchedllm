950442c9728a329e2bcab7edde2ce068abb3331d
Danil (@nenil) <git@nenil.dev>
2026-05-15 16:03:21 +0200

    feat: batch add_messages can now add only user and fixed bug when adding messages directly

--- src/batchedllm/batch.py
+++ src/batchedllm/batch.py
@@ -11,15 +11,23 @@ class Batch:
 
     @overload
     def add_messages(
-        self, messages_or_user: str, assistant: str, *, system_prompt: str | None = None
+        self,
+        messages_or_user: str,
+        assistant: None = ...,
+        *,
+        system_prompt: str | None = ...,
+    ): ...
+    @overload
+    def add_messages(
+        self, messages_or_user: str, assistant: str, *, system_prompt: str | None = ...
     ): ...
     @overload
     def add_messages(
         self,
         messages_or_user: list,
-        assistant: None = None,
+        assistant: None = ...,
         *,
-        system_prompt: str | None = None,
+        system_prompt: str | None = ...,
     ): ...
     def add_messages(
         self,
@@ -28,14 +36,35 @@ class Batch:
         *,
         system_prompt: str | None = None,
     ):
-        if isinstance(messages_or_user, str) and isinstance(assistant, str):
+        """
+        adds new batch, intendet for in-context learning
+
+        system_prompt can override global_system_prompt
+
+        accepts:
+        * (str, None) -> adds string as user message (for testing)
+        * (str, str)  -> adds strings as user and assistant messages (for training)
+        * ([], None)  -> adds list as provided messages (for other uses)
+        """
+
+        sys_prompt = system_prompt or self.global_system_prompt
+
+        if isinstance(messages_or_user, str) and assistant is None:
+            # (str, None) case
+            messages = [
+                {"role": "user", "content": messages_or_user},
+            ]
+
+            if sys_prompt:
+                messages.insert(0, {"role": "system", "content": sys_prompt})
+
+        elif isinstance(messages_or_user, str) and isinstance(assistant, str):
             # (str, str) case
             messages = [
                 {"role": "user", "content": messages_or_user},
                 {"role": "assistant", "content": assistant},
             ]
 
-            sys_prompt = system_prompt or self.global_system_prompt
             if sys_prompt:
                 messages.insert(0, {"role": "system", "content": sys_prompt})
 
@@ -44,18 +73,20 @@ class Batch:
             if len(messages_or_user) == 0:
                 raise ValueError("Can't add empty messages")
 
-            if system_prompt:
-                if messages_or_user[0]["role"] != "system":
-                    messages = [
-                        {"role": "system", "content": system_prompt}
-                    ] + messages_or_user
-                else:
+            if sys_prompt:
+                if messages_or_user[0]["role"] == "system":
                     raise ValueError("Can't set system message if one already exists")
+
+                messages = [
+                    {"role": "system", "content": sys_prompt}
+                ] + messages_or_user
             else:
                 messages = messages_or_user
 
         else:
-            raise TypeError("Unknown combination of arguments")
+            raise TypeError(
+                f"Unknown type combination of arguments. Expected (str, None), (str, str) or (list, None), got: ({type(messages_or_user)}, {assistant})"
+            )
 
         self._messages.append(messages)
 
